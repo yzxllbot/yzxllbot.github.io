03396675166f05ffc7f0535742ff9f1b88cdc603
YiZhiXiaoLiuLang <3035969948@qq,com>
2025-03-09 11:00:53 +0800

    bot-commit-v1

--- uptogit.bat
+++ uptogit.bat
@@ -1,3 +1,5 @@
+cd C:\Users\Administrator\Desktop\runpy\output
+git remote set-url origin git@github.com:yzxllbot/yzxllbot.github.io.git
 git add .
 git commit -m "bot-commit-v1"
 git push -f origin master
